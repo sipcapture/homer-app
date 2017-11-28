3c49b7e9e0177024104517b8556b67d0a94e33a5
Sergii <sergibondarenko@gmail.com>
2017-11-28 22:14:02 +0100

    Add shurik user to the seeds data

--- seeds/01_Users.js
+++ seeds/01_Users.js
@@ -1,4 +1,5 @@
-var bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt');
+const uuidv4 = require('uuid/v4');
 
 exports.seed = function seed(knex) {
   const tableName = 'users';
@@ -12,6 +13,13 @@ exports.seed = function seed(knex) {
       hash: bcrypt.hashSync('password', salt),
       email: 'trex@email.com',
       guid: 'f03ede7c-b121-4112-bcc7-130a3e87988c',
+    },
+    {
+      name: 'Alexandr Dubovikov',
+      username: 'shurik',
+      hash: bcrypt.hashSync('123456', salt),
+      email: 'shurik@email.com',
+      guid: uuidv4(),
     }
   ];
 

e17dedb71f1876d427fa110faaf9bd8a28ef33a3
Sergey Bondarenko <sergibondarenko@gmail.com>
2018-03-13 23:57:44 +0100

    There are responses where data incapsulated into data. Handle this.

--- public/app/dashboards/services/dashboard-storage/dashboard-storage.service.js
+++ public/app/dashboards/services/dashboard-storage/dashboard-storage.service.js
@@ -1,3 +1,5 @@
+import {has} from 'lodash';
+
 class DashboardStorage {
   constructor($http, $rootScope, API) {
     this.$http = $http;
@@ -7,13 +9,13 @@ class DashboardStorage {
 
   get(id) {
     return this.$http.get(this.API.DASHBOARD.STORE+'/'+id).then(function(response) {
-      return response ? response.data || [] : [];
+      return has(response, 'data.data') ? response.data.data : response.data || [];
     });
   }
 
   set(id, data) {
     return this.$http.post(this.API.DASHBOARD.STORE+'/'+id, data).then(function(response) {
-      return response ? response.data || [] : [];
+      return has(response, 'data.data') ? response.data.data : response.data || [];
     });
   }
 
@@ -25,25 +27,25 @@ class DashboardStorage {
 
   update(id, data) {
     return this.$http.put(this.API.DASHBOARD.STORE+'/'+id, data).then(function(response) {
-      return response;
+      return has(response, 'data.data') ? response.data.data : response.data || [];
     });
   }
 
   menu(id, data) {
     return this.$http.post(this.API.DASHBOARD.MENU+'/'+id, data).then(function(response) {
-      return response ? response.data || [] : [];
+      return has(response, 'data.data') ? response.data.data : response.data || [];
     });
   }
 
   getInfoByID(id) {
     return this.$http.get(this.API.DASHBOARD.INFO+'/'+id).then(function(response) {
-      return response ? response.data || [] : [];
+      return has(response, 'data.data') ? response.data.data : response.data || [];
     });
   }
 
   getAll() {
     return this.$http.get(this.API.DASHBOARD.INFO).then(function(response) {
-      return response ? response.data || [] : [];
+      return has(response, 'data.data') ? response.data.data : response.data || [];
     });
   }
 }

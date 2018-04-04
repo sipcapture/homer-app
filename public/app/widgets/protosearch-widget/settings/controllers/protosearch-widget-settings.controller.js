6108c99491f8fe3d288aac852a7c116602b5500b
Alexandr Dubovikov <alexandr.dubovikov@gmail.com>
2018-04-05 00:28:43 +0200

    added types of proto

--- public/app/widgets/protosearch-widget/settings/controllers/protosearch-widget-settings.controller.js
+++ public/app/widgets/protosearch-widget/settings/controllers/protosearch-widget-settings.controller.js
@@ -1,5 +1,6 @@
 import dataHeaders from '../data/headers';
 import dataHeadersUserExtCr from '../data/headers_user_ext_cr';
+import dataProtoTransactions from '../data/proto_transactions';
 
 class ProtosearchWidgetSettings {
   constructor(CONFIGURATION) {
@@ -11,6 +12,9 @@ class ProtosearchWidgetSettings {
     this.counter = 0;
     this.widget = this.resolve.widget;
     this.headers = dataHeaders;
+    this.protoTransactions = dataProtoTransactions;
+    
+    this.addDataToProto();
 
     if (this.CONFIGURATION.USER_EXT_CR) {
       this.headers = this.headers.concat(dataHeadersUserExtCr);
@@ -23,6 +27,40 @@ class ProtosearchWidgetSettings {
   
   submit() {
     this.modalInstance.close(this.widget);
+  }
+
+  addDataToProto() {
+    this.protoData = [];
+    let hashLocal = [];
+    this.protoTransactions.forEach((field) => {
+      if (hashLocal.indexOf(field.id) == -1) {
+        let lobj = {
+          name: field.id,
+          value: field.id,
+        };
+        this.protoData.push(lobj);
+        hashLocal.push(field.id);
+      }
+    });
+    this.ProfileSelect();
+  }
+
+  onProtoSelect() {
+    this.profileData = [];
+    let id = this.widget.config.protocol_id.value;
+    this.protoTransactions.forEach((field) => {
+      if (field.id == id) {
+        let lobj = {
+          name: field.transaction,
+          value: field.transaction,
+        };
+        this.profileData.push(lobj);
+      }
+    });
+  }
+  
+  onProfileSelect() {
+  
   }
 
   addField() {

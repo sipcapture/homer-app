e9bb749d6a4af1a6f5ecc4170b86b4bb33a55e7d
Alexandr Dubovikov <alexandr.dubovikov@gmail.com>
2018-04-24 00:30:41 +0200

    removed proxy

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -136,4 +136,31 @@ export default function dashboards(server) {
         });
     },
   });
+  
+  server.route({
+
+    path: '/api/v3/dashboard/node',
+    method: 'GET',
+    config: {
+      auth: {
+        strategy: 'token',
+      },
+    },
+    handler: function(request, reply) {
+      let userObject = request.auth.credentials;      
+      const settings = new Settings(server, userObject.username);
+      let dashboardId = encodeURIComponent(request.params.dashboardId);
+        
+      let table = 'user_settings';
+      settings.getDashboardList(table, ['id', 'username', 'gid', 'category', 'param', 'create_date', 'data'])
+        .then(function(data) {
+          if (!data) {
+            return reply(Boom.notFound('dashboard was not found'));
+          }
+          return reply(data);
+        }).catch(function(error) {
+          return reply(Boom.serverUnavailable(error));
+      });
+    },
+  });
 };

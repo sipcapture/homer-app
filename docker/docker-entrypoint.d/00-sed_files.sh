b92c89a76d26e1e8156308a2e0ae30b1e01bf8d6
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2018-07-02 19:30:27 +0200

    fix entrypoint

--- docker/docker-entrypoint.d/00-sed_files.sh
+++ docker/docker-entrypoint.d/00-sed_files.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ -f /app/server/config.js ]; then
-   sed -i 's/localhost/"$DB_HOST"/g' /app/server/config.js
-   sed -i 's/homer_user/"$DB_USER"/g' /app/server/config.js
-   sed -i 's/homer_password/"$DB_PASS"/g' /app/server/config.js
+   sed -i 's/localhost/${DB_HOST}/g' /app/server/config.js
+   sed -i 's/homer_user/${DB_USER}/g' /app/server/config.js
+   sed -i 's/homer_password/${DB_PASS}/g' /app/server/config.js
    echo "Pre-Flight provisioning completed!"
 else
    echo "Configuration file not found!"

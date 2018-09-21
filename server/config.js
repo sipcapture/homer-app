ecb628177dccadddde0ab6bef23761f54d0cea46
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2018-09-21 14:15:13 +0200

    Update config.js

--- server/config.js
+++ server/config.js
@@ -39,7 +39,7 @@ export default {
   http_host: '0.0.0.0',
   http_port: 80,
   https_host: '0.0.0.0',
-  https_port: 433,
+  https_port: 443,
   certificate: {
     self_signed: true,
     days: 1,

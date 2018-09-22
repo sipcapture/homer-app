ab586ffe6873d34537e99b8154b762b40f4a8b58
Sergii Bondarenko <sergibondarenko@gmail.com>
2018-09-22 14:21:52 +0200

    Delete non-existent provider.

--- public/app/services/user-settings-service/index.js
+++ public/app/services/user-settings-service/index.js
@@ -2,4 +2,4 @@ import angular from 'angular';
 import UserSettingsService from './user-settings.service';
 
 export default angular.module('hepicApp.services.user-settings', [])
-  .factory('UserSettingsService', /* @ngInject */ ($http, API, APP_PREF_SCHEMA) => new UserSettingsService($http, API, APP_PREF_SCHEMA));
+  .factory('UserSettingsService', /* @ngInject */ ($http, API) => new UserSettingsService($http, API));

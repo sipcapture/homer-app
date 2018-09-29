af16d5b0b9872f9f4e67971f01552a28c392a3ce
Sergii Bondarenko <sergibondarenko@gmail.com>
2018-09-29 19:44:16 +0200

    Pass time as URL parameter.

--- public/app/search/call-search/index.js
+++ public/app/search/call-search/index.js
@@ -4,9 +4,13 @@ import component from './call-search.component';
 export default angular.module('callSearch', [])
   .config(function($stateProvider, ROUTER, SEARCH) {
     'ngInject';
+
     $stateProvider.state(ROUTER.SEARCH.NAME, {
-      url: ROUTER.SEARCH.PATH + '/:protoID/{limit:int}/{search:json}/{transaction:json}',
+      url: ROUTER.SEARCH.PATH + '/:protoID/{limit:int}/{from:int}/{to:int}/{timezone:json}/{search:json}/{transaction:json}',
       params: {
+        to: SEARCH.TO,
+        from: SEARCH.FROM,
+        timezone: SEARCH.TIMEZONE,
         limit: SEARCH.LIMIT,
         search: SEARCH.QUERY.DEFAULT,
         transaction: SEARCH.TRANSACTION.DEFAULT

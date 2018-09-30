9cd22561637fc1f5b2e72a48d6e5396bef181f49
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2018-10-01 00:33:27 +0200

    RTCP basic stats (#42)
    * Calculate Series max/min/avg

--- public/app/search/call-detail/call-detail-qos/call-detail-qos.controller.js
+++ public/app/search/call-detail/call-detail-qos/call-detail-qos.controller.js
@@ -14,6 +14,7 @@ class CallDetailQos {
     this._reports = [];
     this._labels = [];
     this.reportData = [];
+    this._stats = {};
   }
 
   async $onInit() {
@@ -79,8 +80,15 @@ class CallDetailQos {
   }
 
   _aggregateAllStats() {
+    var banlist = ['packets', 'octets', 'highest_seq_no'];
     this._labels.forEach((label) => { 
       this.reportData.push(this._reports[label][0])
+      if (banlist.includes(label)) return;
+      this._stats[label] = {
+        min: this._reports[label][0].values.reduce((min, p) => p.y < min ? p.y : min, this._reports[label][0].values[0].y),
+        avg: parseInt(this._reports[label][0].values.reduce((tot, p) => (tot + p.y) / this._reports[label][0].values.length , 0)),
+        max: this._reports[label][0].values.reduce((max, p) => p.y > max ? p.y : max, this._reports[label][0].values[0].y)
+      }
     });
   }
 

6928da1bca5fa1d95442170c4e82387ee21991dd
Sergii Bondarenko <sergibondarenko@gmail.com>
2018-09-30 18:44:04 +0200

    Create an empty QoS tab.

---
+++ public/app/search/call-detail/call-detail-qos/index.js
@@ -0,0 +1,4 @@
+import angular from 'angular';
+import component from './call-detail-qos.component';
+
+export default angular.module('hepicApp.call-detail-qos', []).component('callDetailQos', component);

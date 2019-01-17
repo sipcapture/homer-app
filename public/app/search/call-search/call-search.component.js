ed711bc8a8aa77aac4c8077a95d9e2a049e64ec2
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2019-01-17 15:37:37 +0100

    add css for call-search

--- public/app/search/call-search/call-search.component.js
+++ public/app/search/call-search/call-search.component.js
@@ -1,6 +1,8 @@
 import template from './templates/call-search.template.html';
 import controller from './controllers/call-search.controller';
 
+import './templates/call-search.template.css';
+
 const callSearch = {
   controller,
   template,

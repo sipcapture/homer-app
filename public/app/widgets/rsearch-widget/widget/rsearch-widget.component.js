eb08b302130d551a4c40b48718514662f7cbadba
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2019-01-20 02:37:33 +0100

    Implement LogQL dynamic typing in Ace (#56)
    Merge nightly changes to LogQL Loki editor, Ace Autocomplete, CSS adjustments

--- public/app/widgets/rsearch-widget/widget/rsearch-widget.component.js
+++ public/app/widgets/rsearch-widget/widget/rsearch-widget.component.js
@@ -1,6 +1,8 @@
 import template from './templates/rsearch-widget.template.html';
 import controller from './controllers/rsearch-widget.controller';
 
+import './templates/rsearch-widget.template.css';
+
 const rsearchWidget = {
   bindings: {
     widget: '<',

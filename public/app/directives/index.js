185fb08e7974d762f9dcf15390779e46dbc3d6ce
Sergii Bondarenko <sergibondarenko@gmail.com>
2019-01-21 11:45:45 +0100

    Add autoFillSync directive to update ng-model in case of input autofilling by a browser. (#57)

--- public/app/directives/index.js
+++ public/app/directives/index.js
@@ -1,8 +1,10 @@
 import angular from 'angular';
 import JsonEditor from './json-editor';
 import PwCheck from './pw-check';
+import AutoFillSync from './auto-fill-sync';
 
 export default angular.module('hepicApp.directives', [
   JsonEditor.name,
   PwCheck.name,
+  AutoFillSync.name
 ]);

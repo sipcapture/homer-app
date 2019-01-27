452fd1d02a91dd7d1b2730db777a24c895b147b2
Alexandr Dubovikov <alexandr.dubovikov@gmail.com>
2019-01-27 12:21:37 +0100

    add global profile

---
+++ public/app/services/global-profile/global-profile.service.js
@@ -0,0 +1,69 @@
+import {forEach, pick} from 'lodash';
+import Promise from 'bluebird';
+
+class GlobalProfile {
+  constructor($http, $log, API, TimeMachine, SEARCH) {
+    this.$http = $http;
+    this.$log = $log;
+    this.API = API;
+    this.profileScope = {};
+    this.loadedProfile = false;
+    this.factory = {};
+    this.myServerProfile = {};
+    this.myProfile = pick(this.profileScope, []);
+  }
+
+  key(obj) {
+    return obj.lastName + obj.firstName; // just an example
+  }
+          
+  getProfile(key) {
+    return this.getLocalProfile(key);
+  }
+  
+  getProfileCategory(key1,key2) {
+    return this.getLocalProfile(key1+":"+key2);
+  }
+      
+  setLocalProfile(key, data) {
+    this.myProfile[key] = data;
+  }
+      
+  getLocalProfile(key) {
+    return this.myProfile[key];
+  }
+  
+  getLocalProfileCategory(key1,key2) {
+    return this.myProfile[key1+":"+key2];
+  }
+      
+  getAllRemoteProfile() {
+    return this.$http.get(this.API.GLOBALPROFILE.STORE, {handleStatus: [403, 503]}).then((response) => {
+      forEach(response.data.data, (value, key) => {
+           var nkey=value.category+":"+value.param;
+           var nobj = JSON.parse(value.data);           
+           this.setLocalProfile(nkey, nobj);
+           this.profileScope[nkey] = nobj;
+           console.log("VV", nkey);
+           console.log("DD", nobj);
+           this.loadedProfile = true;
+           return 'yes';
+      });      
+    });
+  }
+          
+  getRemoteProfile(id) {
+    return this.$http.get(this.API.GLOBALPROFILE.STORE+'/' + id, {handleStatus: [403, 503]}).then(function(response) {
+      return response.data;
+    });
+  }
+          
+  getAll() {
+    if (this.loadedProfile == true) {
+      return Promise.resolve(this.loadedProfile);
+    }
+    return this.getAllRemoteProfile();
+  }
+}
+
+export default GlobalProfile;

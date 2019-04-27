6193074297aa462a7e3c4be4b2bb4b7a8b435f69
Alexandr Dubovikov <alexandr.dubovikov@gmail.com>
2019-04-27 13:58:57 +0200

    fixed jquery and added AgentLocation

---
+++ seeds/05_AgentLocation.js
@@ -0,0 +1,30 @@
+const uuidv4 = require('uuid/v4');
+
+exports.seed = function seed(knex) {
+  const tableName = 'agent_location_session';
+
+  let empty = {};
+  
+  const rows = [
+    {
+      guid: uuidv4(),
+      gid: 10,
+      host: 'agent',
+      port: 8080,
+      protocol: 'rtp',
+      path: '/api/search',
+      node: 'rtpnode01',
+      type: 'rtp',
+      create_date: new Date(),
+      expire_date: new Date('December 31, 2032 00:00:00'),
+      active: 1,
+    },
+  ];
+
+  return knex(tableName)
+    // Empty the table (DELETE)
+    .del()
+    .then(function() {
+      return knex.insert(rows).into(tableName);
+    });
+};

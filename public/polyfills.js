dd9c905850edc822e91c661e9b2ce3ec669633c2
Alexandr Dubovikov <alexandr.dubovikov@gmail.com>
2019-11-09 15:10:47 +0100

    added new build

--- public/polyfills.js
+++ public/polyfills.js
@@ -6415,8 +6415,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /usr/src/hepic-ui-3/src/polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! /usr/src/hepic-ui-3/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! /home/shurik/Projects/hepic-ui-3/src/polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /home/shurik/Projects/hepic-ui-3/node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })

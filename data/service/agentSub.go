8ca9600e5a10577920a8074e66b3c09d0c193ba6
Callum <callum.guy@x-on.co.uk>
2022-12-16 14:03:21 +0000

    :art: code refactor group similar functions together, provide improved method comments

--- data/service/agentSub.go
+++ data/service/agentSub.go
@@ -23,7 +23,29 @@ type AgentsubService struct {
 	ServiceConfig
 }
 
-// this method gets all users from database
+// GetAgentsub gets all active HEPSUB agent sessions from database
+func (hs *AgentsubService) GetAgentsub() (string, error) {
+	var AgentsubObject []model.TableAgentLocationSession
+	var count int
+	if err := hs.Session.Debug().Table("agent_location_session").
+		Find(&AgentsubObject).Count(&count).Error; err != nil {
+		return "", err
+	}
+
+	sort.Slice(AgentsubObject[:], func(i, j int) bool {
+		return AgentsubObject[i].GUID < AgentsubObject[j].GUID
+	})
+
+	response, _ := json.Marshal(AgentsubObject)
+	dataElement, _ := gabs.ParseJSON(response)
+
+	reply := gabs.New()
+	reply.Set("successfully retrieved agent records", "message")
+	reply.Set(dataElement.Data(), "data")
+	return reply.String(), nil
+}
+
+// GetAgentsubAgainstGUID gets a specific active HEPSUB agent session from database identified by GUID
 func (hs *AgentsubService) GetAgentsubAgainstGUID(guid string) (string, error) {
 	var AgentsubObject []model.TableAgentLocationSession
 	var count int
@@ -48,7 +70,7 @@ func (hs *AgentsubService) GetAgentsubAgainstGUID(guid string) (string, error) {
 	return reply.String(), nil
 }
 
-// this method gets all users from database
+// GetAgentsubAgainstType gets all active HEPSUB agent sessions from database for a given type (pattern matched)
 func (hs *AgentsubService) GetAgentsubAgainstType(typeRequest string) (string, error) {
 	var AgentsubObject []model.TableAgentLocationSession
 	var count int
@@ -79,7 +101,23 @@ func (hs *AgentsubService) GetAgentsubAgainstType(typeRequest string) (string, e
 	return reply.String(), nil
 }
 
-// this method gets all users from database
+// GetAgentsubAgainstGUIDAndType gets an active HEPSUB agent session by GUID for a given type (pattern matched)
+func (hs *AgentsubService) GetAgentsubAgainstGUIDAndType(guid string, typeRequest string) (model.TableAgentLocationSession, error) {
+	var AgentsubObject model.TableAgentLocationSession
+	var count int
+
+	whereSQL := fmt.Sprintf("expire_date > NOW() AND guid = '%s' AND type LIKE '%%%s%%'", guid, typeRequest)
+
+	if err := hs.Session.Debug().Table("agent_location_session").
+		Where(whereSQL).
+		Find(&AgentsubObject).Count(&count).Error; err != nil {
+		return AgentsubObject, err
+	}
+
+	return AgentsubObject, nil
+}
+
+// GetAuthKeyByHeaderToken gets user auth tokens from database
 func (hs *AgentsubService) GetAuthKeyByHeaderToken(token string) (string, error) {
 	var tokenObject model.TableAuthToken
 	var count int
@@ -108,28 +146,7 @@ func (hs *AgentsubService) GetAuthKeyByHeaderToken(token string) (string, error)
 	return reply.String(), nil
 }
 
-// this method gets all users from database
-func (hs *AgentsubService) GetAgentsub() (string, error) {
-	var AgentsubObject []model.TableAgentLocationSession
-	var count int
-	if err := hs.Session.Debug().Table("agent_location_session").
-		Find(&AgentsubObject).Count(&count).Error; err != nil {
-		return "", err
-	}
-	sort.Slice(AgentsubObject[:], func(i, j int) bool {
-		return AgentsubObject[i].GUID < AgentsubObject[j].GUID
-	})
-
-	response, _ := json.Marshal(AgentsubObject)
-	dataElement, _ := gabs.ParseJSON(response)
-
-	reply := gabs.New()
-	reply.Set("successfully created agent record", "message")
-	reply.Set(dataElement.Data(), "data")
-	return reply.String(), nil
-}
-
-// this method gets all users from database
+// AddAgentsub creates a new HEPSUB agent session
 func (hs *AgentsubService) AddAgentsub(data model.TableAgentLocationSession) (string, error) {
 	if err := hs.Session.Debug().Table("agent_location_session").
 		Create(&data).Error; err != nil {
@@ -140,13 +157,14 @@ func (hs *AgentsubService) AddAgentsub(data model.TableAgentLocationSession) (st
 	sidData.Set(data.ExpireDate, "expire_date")
 	sidData.Set(data.ExpireDate.Unix(), "expire_ts")
 	sidData.Set(data.GUID, "uuid")
+
 	reply := gabs.New()
 	reply.Set("successfully created agent record", "message")
 	reply.Set(sidData.Data(), "data")
 	return reply.String(), nil
 }
 
-// this method gets all users from database
+// UpdateAgentsubAgainstGUID updates an existing HEPSUB agent session
 func (hs *AgentsubService) UpdateAgentsubAgainstGUID(guid string, data model.TableAgentLocationSession) (string, error) {
 	if err := hs.Session.Debug().Table("agent_location_session").
 		Where("guid = ?", guid).
@@ -157,7 +175,7 @@ func (hs *AgentsubService) UpdateAgentsubAgainstGUID(guid string, data model.Tab
 	return response, nil
 }
 
-// this method gets all users from database
+// DeleteAgentsubAgainstGUID removes an existing HEPSUB agent session
 func (hs *AgentsubService) DeleteAgentsubAgainstGUID(guid string) (string, error) {
 	var AgentsubObject []model.TableAgentLocationSession
 	if err := hs.Session.Debug().Table("agent_location_session").
@@ -170,39 +188,20 @@ func (hs *AgentsubService) DeleteAgentsubAgainstGUID(guid string) (string, error
 	return response, nil
 }
 
-// this method gets all users from database
-func (hs *AgentsubService) GetAgentsubAgainstGUIDAndType(guid string, typeRequest string) (model.TableAgentLocationSession, error) {
-	var AgentsubObject model.TableAgentLocationSession
-	var count int
-
-	whereSQL := fmt.Sprintf("expire_date > NOW() AND guid = '%s' AND type LIKE '%%%s%%'", guid, typeRequest)
-
-	if err := hs.Session.Debug().Table("agent_location_session").
-		Where(whereSQL).
-		Find(&AgentsubObject).Count(&count).Error; err != nil {
-		return AgentsubObject, err
-	}
-
-	return AgentsubObject, nil
-}
-
 // DoSearchByPost loads the subscriber mappings from the database and builds a HEPSUB search query using request data
 func (hs *AgentsubService) DoSearchByPost(agentObject model.TableAgentLocationSession, searchObject model.SearchObject, typeRequest string) ([]byte, error) {
 
+	var lookupField string
 	var hepsubObject []model.TableHepsubSchema
 	Data, _ := json.Marshal(searchObject.Param.Search)
 	sData, _ := gabs.ParseJSON(Data)
 	dataPost := gabs.New()
-	var lookupField string
 
 	// process the search request(s), arriving "[hepid]_[type]" i.e. "1_call"
 	for key := range sData.ChildrenMap() {
-
 		elemArray := strings.Split(key, "_")
-		logger.Debug(elemArray)
-
 		if len(elemArray) != 2 {
-			return nil, fmt.Errorf("agent HEPSUB: key is wrong: %d", len(elemArray))
+			return nil, fmt.Errorf("Agent HEPSUB: key is wrong: %d", len(elemArray))
 		}
 
 		hepID, _ := strconv.Atoi(elemArray[0])
@@ -227,9 +226,7 @@ func (hs *AgentsubService) DoSearchByPost(agentObject model.TableAgentLocationSe
 	lookupFields := sMapping.Search("source_fields")
 
 	for _, value := range sData.ChildrenMap() {
-
 		if lookupFields != nil {
-
 			for k := range lookupFields.ChildrenMap() {
 				dataV := value.Search(k).Data().([]interface{})
 				dataPost.Set(dataV, k)
@@ -238,7 +235,6 @@ func (hs *AgentsubService) DoSearchByPost(agentObject model.TableAgentLocationSe
 			dataV := value.Search("callid").Data()
 			dataPost.Set(dataV, "callid")
 		}
-
 		break
 	}
 
@@ -254,8 +250,8 @@ func (hs *AgentsubService) DoSearchByPost(agentObject model.TableAgentLocationSe
 	}
 
 	lookupField = strings.ReplaceAll(lookupField, "$source_field", dataPost.String())
-	lookupField = strings.ReplaceAll(lookupField, "$fromts", fmt.Sprintf("%v", (timeFrom.Unix()*int64(time.Microsecond)+(int64(timeFrom.Nanosecond())/int64(time.Microsecond)))))
-	lookupField = strings.ReplaceAll(lookupField, "$tots", fmt.Sprintf("%v", (timeTo.Unix()*int64(time.Microsecond)+(int64(timeTo.Nanosecond())/int64(time.Microsecond)))))
+	lookupField = strings.ReplaceAll(lookupField, "$fromts", fmt.Sprintf("%v", timeFrom.Unix()*int64(time.Microsecond)+(int64(timeFrom.Nanosecond())/int64(time.Microsecond))))
+	lookupField = strings.ReplaceAll(lookupField, "$tots", fmt.Sprintf("%v", timeTo.Unix()*int64(time.Microsecond)+(int64(timeTo.Nanosecond())/int64(time.Microsecond))))
 
 	serverURL := fmt.Sprintf("%s://%s:%d%s/%s", agentObject.Protocol, agentObject.Host, agentObject.Port, agentObject.Path, typeRequest)
 	serverNODE := agentObject.Node
@@ -285,6 +281,7 @@ func (hs *AgentsubService) DoSearchByPost(agentObject model.TableAgentLocationSe
 		return nil, err
 	}
 
+	// when downloading the file, return only the file data
 	if typeRequest == "download" {
 		var buffer bytes.Buffer
 		export := exportwriter.NewWriter(buffer)
@@ -293,15 +290,14 @@ func (hs *AgentsubService) DoSearchByPost(agentObject model.TableAgentLocationSe
 			logger.Error("write error to the download buffer", err)
 		}
 		return export.Buffer.Bytes(), nil
-
-	} else {
-		responseData, _ := gabs.ParseJSON(buf)
-		reply := gabs.New()
-		reply.Set("request answer", "message")
-		reply.Set(serverNODE, "node")
-		reply.Set(agentObject.GUID, "uuid")
-		reply.Set(responseData.Data(), "data")
-		return reply.Bytes(), nil
 	}
 
+	// respond to lookup requests
+	responseData, _ := gabs.ParseJSON(buf)
+	reply := gabs.New()
+	reply.Set("request answer", "message")
+	reply.Set(serverNODE, "node")
+	reply.Set(agentObject.GUID, "uuid")
+	reply.Set(responseData.Data(), "data")
+	return reply.Bytes(), nil
 }
